b284e96ff99dafd14812f9dc058247c8754a6d15
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2023-01-08 17:10:47 +0100

    finito mapper e iniettato in App

--- lib/app.dart
+++ lib/app.dart
@@ -2,39 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:matches/pages/main_page.dart';
+import 'package:matches/repositories/mappers/match_mapper.dart';
+import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Partite',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        scaffoldBackgroundColor: Colors.grey.shade300,
-        appBarTheme: const AppBarTheme(
-          titleTextStyle: TextStyle(
-              color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
-          backgroundColor: Colors.indigo, //* stessa colorazione
-          elevation: 5,
+    return MultiProvider(
+      providers: [
+        Provider<MatchMapper>(
+          create: (context) => MatchMapper(),
         ),
-        tabBarTheme: const TabBarTheme(
-          unselectedLabelColor: Color(0x88FFFFFF), //tab non sel
-          labelColor: Colors.white,
-          indicator: UnderlineTabIndicator(
-            borderSide: BorderSide(color: Colors.white),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Partite',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          scaffoldBackgroundColor: Colors.grey.shade300,
+          appBarTheme: const AppBarTheme(
+            titleTextStyle: TextStyle(
+                color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+            backgroundColor: Colors.indigo, //* stessa colorazione
+            elevation: 5,
           ),
-          labelStyle: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        cardTheme: CardTheme(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16.0),
+          tabBarTheme: const TabBarTheme(
+            unselectedLabelColor: Color(0x88FFFFFF), //tab non sel
+            labelColor: Colors.white,
+            indicator: UnderlineTabIndicator(
+              borderSide: BorderSide(color: Colors.white),
             ),
-            elevation: 1),
+            labelStyle: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          cardTheme: CardTheme(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              elevation: 1),
+        ),
+        home: const MainPage(),
       ),
-      home: const MainPage(),
     );
   }
 }
